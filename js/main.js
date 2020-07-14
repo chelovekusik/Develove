634b8b0f0847351c7d0ade0c22dd96a48b48e104
Borius <bobernew@gmail.com>
2020-07-14 17:55:08 +0300

    вторая практика 015

--- js/main.js
+++ js/main.js
@@ -1,24 +1,52 @@
-let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели", "");
+   "use strict";
 
-console.log(numberOfFilms);
+   let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели", "");
+
+   console.log(numberOfFilms);
+   
+   
+   
+   let personalMovieDB = {
+     count: numberOfFilms,
+     movies: {},
+     actors: {},
+     genres: [],
+     privat: false,
+   };
+   
+
+   for (let i = 0; i < 2; i++) {
+    const a = prompt("Один из последних просмотренных фильмов", ""),
+        b = prompt("На сколько оцените его", "");
+        
+        if (a != null && b != null && a != "" && b != ""  && a.length < 50) { //Переменая a не должна равняться null т.е кнопеи отмены и b тоже не должната раняться этому или эти переменные не должны равнятья пустой строке
+        personalMovieDB.movies[a] = b;// переменая a-свойство нашего объекта b-значение этого свойства
+          console.log("done");
+        } else {
+          console.log("error");
+          i--;
+        }
+   }
+
+   
+
+   console.log(personalMovieDB);
+
+   if (personalMovieDB.count < 10) {
+     alert("Просмотрено довольно мало фильмов");
+   } else if (personalMovieDB.count <= 30) {
+    alert("Вы класический зритель");
+   } else if (personalMovieDB.count > 30) {
+      alert("Вы киноман");
+   } else {
+     alert("Произошла ошибка");
+   }
+   
+
+   
+
+ 
 
 
 
-let personalMovieDB = {
-  count: numberOfFilms,
-  movies: {},
-  actors: {},
-  genres: [],
-  privat: false,
-};
-
-let a = prompt("Один из последних просмотренных фильмов", ""),
-    b = prompt("На сколько оцените его", "");
-
-let c = prompt("Один из последних просмотренных фильмов", "");
-let d = prompt("На сколько оцените его", "");
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-console.log(personalMovieDB);
 
